929e1527a971384485ce475a54fa1e1a4ef99402
PJ Lindsay <pjlindsay@gmail.com>
2025-11-23 13:37:52 -0700

    Add Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Header from './components/Header'
 import goalsImg from './assets/goals.jpg'
+import CourseGoals from './components/CourseGoals.tsx'
 
 function App() {
   return (
@@ -7,6 +8,21 @@ function App() {
       <Header image={{ src: goalsImg, alt: 'A list of goals' }}>
         <h1>Your Course goals</h1>
       </Header>
+
+      <CourseGoals
+        goals={[
+          {
+            id: 1,
+            title: 'Learn TS',
+            description: 'Learn TS from the ground up',
+          },
+          {
+            id: 2,
+            title: 'Practice TS',
+            description: 'Practice working with TypeScript!',
+          },
+        ]}
+      />
     </main>
   )
 }
