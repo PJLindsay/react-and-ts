822b5478839adadfe9de3dba83f18da1a7b88d01
PJ Lindsay <pjlindsay@gmail.com>
2025-11-23 13:47:28 -0700

    Add Delete

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,34 @@
+import { useState } from 'react'
+
 import Header from './components/Header'
 import goalsImg from './assets/goals.jpg'
 import CourseGoals from './components/CourseGoals.tsx'
 
 function App() {
+  const [goals, setGoals] = useState([
+    {
+      id: 1,
+      title: 'Learn TS',
+      description: 'Learn TS from the ground up',
+    },
+    {
+      id: 2,
+      title: 'Practice TS',
+      description: 'Practice working with TypeScript!',
+    },
+  ])
+
+  function handleDeleteGoal(id: number) {
+    setGoals((prevGoals) => prevGoals.filter((g) => g.id !== id))
+  }
+
   return (
     <main>
       <Header image={{ src: goalsImg, alt: 'A list of goals' }}>
         <h1>Your Course goals</h1>
       </Header>
 
-      <CourseGoals
-        goals={[
-          {
-            id: 1,
-            title: 'Learn TS',
-            description: 'Learn TS from the ground up',
-          },
-          {
-            id: 2,
-            title: 'Practice TS',
-            description: 'Practice working with TypeScript!',
-          },
-        ]}
-      />
+      <CourseGoals goals={goals} onDelete={handleDeleteGoal} />
     </main>
   )
 }
