0ff05b6e0b43dfa7714f12f7a6700946d61a274b
PJ Lindsay <pjlindsay@gmail.com>
2025-11-23 13:59:13 -0700

    Handle form submission

--- src/components/NewGoal.tsx
+++ src/components/NewGoal.tsx
@@ -1,20 +1,27 @@
-import { FormEvent } from 'react'
+import { useRef, type FormEvent } from 'react'
 
 export default function NewGoal() {
+  const goalRef = useRef<HTMLInputElement>(null)
+  const summaryRef = useRef<HTMLInputElement>(null)
+
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
 
+    const enteredGoal = goalRef.current!.value
+    const enteredSummary = summaryRef.current!.value
+
+    // validation...
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <p>
         <label htmlFor="goal">Your goal</label>
-        <input id="goal" type="text" />
+        <input id="goal" type="text" ref={goalRef} />
       </p>
       <p>
         <label htmlFor="summary">Short summary</label>
-        <input id="summary" type="text" />
+        <input id="summary" type="text" ref={summaryRef} />
       </p>
       <p>
         <button>Add Goal</button>
